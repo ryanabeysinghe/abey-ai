7a15b7a4f1c87214e571d43da97f910d831c9945
Ryan Abeysinghe <ryanabey@terpmail.umd.edu>
2025-03-08 17:51:18 -0500

    dashboard ui changes

--- src/routes/dashboard/Dashboard.jsx
+++ src/routes/dashboard/Dashboard.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 const Dashboard = () => {
   return (
-    <div>dashboard</div>
-  )
+    <div>
+      <div>
+        <div>
+          <img src="/logo.png" alt="ABEY AI Logo" />
+          <h1 className='uppercase'>abey ai</h1>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
