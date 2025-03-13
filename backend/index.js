9c86ce864f0b41bdaab3856d8e9376c4bcf1888e
Ryan Abeysinghe <ryanabey@terpmail.umd.edu>
2025-03-13 16:03:58 -0400

    create express backend server

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+/* This file serves to run Express server */
+
+/* 
+Changed scripts package.json to add ("start": "nodemon index.js") so nodemon can be used for real-time changes
+*/
+
+//import express from "express";
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000;
+
+const ImageKit = require('imagekit');
+
+const imagekit = new ImageKit({
+    urlEndpoint: process.env.IMAGE_KIT_URL_ENDPOINT,
+    publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGE_KIT_PRIVATE_KEY
+});
+
+// Represents first API Endpoint (GET request)
+app.get("/api/upload", (req, res) => {
+    const result = imagekit.getAuthenticationParameters();
+    res.send(result);
+})
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+})
+
+
+// console.log("test3");
