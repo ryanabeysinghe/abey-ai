5f200d9e02ada546a839bddec7942d68cd6038e1
Ryan Abeysinghe <ryanabey@terpmail.umd.edu>
2025-04-11 17:38:39 -0400

    finish backend

---
+++ backend/models/userChats.js
@@ -0,0 +1,29 @@
+//const mongoose = require('mongoose');
+import mongoose from "mongoose";
+
+const userChatsSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    chats: [
+        {
+            _id: {
+                type: String,
+                required: true,
+            },
+            title: {
+                type: String,
+                required: true,
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now(),
+            },
+        },
+    ],
+}, {timestamps: true});
+
+export default mongoose.models.userChats || mongoose.model("userChats", userChatsSchema);
+
+// module.exports = mongoose.models.userChats || mongoose.model("userChats", userChatsSchema);
